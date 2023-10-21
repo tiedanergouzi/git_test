136df305ba4c718a97882eb5fe31457969f75de6
tiedanergouzi <757682241@qq.com>
2023-10-21 20:57:03 +0800

    新建第一个

---
+++ src/main/java/com/example/demo/controller/Helloworld.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Helloworld {
+    @RequestMapping("/helloworld")
+    public String hello(){
+        return "hello world";
+    }
+}
